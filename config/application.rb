1bb5c56d8d82cd0d55d71c3341b8a439400eaffe
miloneko <satohannn@gmail.com>
2022-05-10 20:06:56 +0900

    Add:i18nの設定

--- config/application.rb
+++ config/application.rb
@@ -40,6 +40,10 @@ module Wheretogo
       g.helper false
       g.test_framework false
       g.jbuilder false
+
+      config.i18n.default_locale = :ja
+      config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+      config.time_zone = "Tokyo"
     end
   end
 end
