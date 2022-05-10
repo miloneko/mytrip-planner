64ed049bda3367bd313cf7f13b332540391f41b8
miloneko <satohannn@gmail.com>
2022-05-10 20:55:00 +0900

    Add: Postsコントローラーの作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = current_user.posts.build(post_params)
+    if post.save
+      flash[:notice] = t('.success')
+    else
+      render :new
+    end
+  end
+
+  def index
+    @posts = Post.all.includes(:user).order(created_at: :desc)
+  end
+end
