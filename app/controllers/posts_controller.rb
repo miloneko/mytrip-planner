607dc0bd5fa7dacb068ab2f34fbb20dd3fd251fd
miloneko <satohannn@gmail.com>
2022-05-11 11:59:46 +0900

    Add: viewの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:image, :title, :user)
+    params.require(:post).permit(:image, :title, :user, :location_id)
   end
 end
