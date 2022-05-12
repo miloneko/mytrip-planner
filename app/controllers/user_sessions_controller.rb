6f665f338739430e34b09cd24063cc6468a0a721
miloneko <satohannn@gmail.com>
2022-05-12 17:59:13 +0900

    Fix: リントエラー解消

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,4 +1,4 @@
-class UserSessionsController < ApplicationController 
+class UserSessionsController < ApplicationController
   skip_before_action :require_login, only: %i[new create]
   def new; end
 
