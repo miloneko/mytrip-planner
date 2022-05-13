f7e205a8320a276ff4a4a6b5a5a53deeebc61009
miloneko <satohannn@gmail.com>
2022-05-13 19:41:24 +0900

    Add:adminlteの追加

---
+++ app/javascript/application.js
@@ -0,0 +1,9 @@
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
+//= require edit_comment.js
+//= require preview.js
+//= require cable.js
+//= require preview
