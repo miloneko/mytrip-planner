b0fd7e30d2ce802d34bdca326674e389c9a7012f
miloneko <satohannn@gmail.com>
2022-05-16 12:24:48 +0900

    Add: viewの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   root to: 'static_pages#top'
   resources :users, only: %i[new create]
   get 'login', to: 'user_sessions#new'
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
   resources :posts do
     collection do
       get 'search'
+  resources :password_resets, only: %i[new create edit update]
   end
  end
 end
