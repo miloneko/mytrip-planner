b37bf53d5334a84c11f311a6cf502243af013592
miloneko <satohannn@gmail.com>
2022-05-17 18:27:53 +0900

    Add:管理画面

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,24 @@
+class Admin::UsersController < ApplicationController
+  before_action :admin_user, only: %i[index destroy]
+  before_action :set_user, only: %i[destroy]
+
+
+  def index
+    @users = User.all.order(created_at: :desc)
+  end
+
+  def destroy
+    @user.destroy!
+    redirect_to admin_users_path, success: '削除しました'
+  end
+
+  private
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
