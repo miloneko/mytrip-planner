ee908a522b747d7599550733c8ab6fc83dea80f9
miloneko <satohannn@gmail.com>
2022-05-18 12:39:50 +0900

    Add: ページネーションの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.all.includes(:user, :categories).order(created_at: :desc)
+    @posts = Post.all.includes(:user, :categories).order(created_at: :desc).page(params[:page])
   end
 
   def show
