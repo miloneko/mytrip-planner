0e06a4f4fab6c5903aaa4d7ce9f908494ada19ea
miloneko <satohannn@gmail.com>
2022-05-20 00:15:33 +0900

    Add: プライバシーポリシー利用規約の追加利用規約の追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   root to: 'static_pages#top'
   get 'about', to: 'about#show'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'terms', to: 'static_pages#terms'
   resources :users, only: %i[new create]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
