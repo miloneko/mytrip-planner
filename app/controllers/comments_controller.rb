8d323a208c6ff248f329318f608a004f7bd22c29
miloneko <satohannn@gmail.com>
2022-06-09 13:37:35 +0900

    Add: コメント機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,14 @@
 class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.new(comment_params)
-    @comment.save
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:body).merge(picture_id: params[:ppst_id])
+    params.require(:comment).permit(:body).merge(post_id: params[:post_id])
   end
 end
