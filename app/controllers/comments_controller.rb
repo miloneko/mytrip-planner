42048c90babfad2e59a0bfff6fc7df9983244642
miloneko <satohannn@gmail.com>
2022-06-09 14:14:08 +0900

    Add: コメント消去

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,11 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy!
+  end
+
   private
 
   def comment_params
