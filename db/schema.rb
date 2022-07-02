106d796a115f18623b1a07acdb07dcb8df99df1f
miloneko <satohannn@gmail.com>
2022-07-02 18:35:16 +0900

    Add:ダミーデータ用のテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_23_094434) do
+ActiveRecord::Schema.define(version: 2022_07_02_092933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,15 @@ ActiveRecord::Schema.define(version: 2022_06_23_094434) do
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "samples", force: :cascade do |t|
+    t.string "image"
+    t.string "title", null: false
+    t.integer "location_id"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
