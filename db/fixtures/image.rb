33a8f74fa050d6fe31b6f14b9cb6cccf2677c493
miloneko <satohannn@gmail.com>
2022-07-12 23:35:52 +0900

    Fix: データ追加

--- db/fixtures/image.rb
+++ db/fixtures/image.rb
@@ -6,4 +6,9 @@ Sample.seed do |sample|
   sample.id = 2
   sample.image = Rails.root.join("db/fixtures/images/img_2.jpg").open
 end
+Sample.seed do |sample|
+  sample.id = 3
+  sample.image = Rails.root.join("db/fixtures/images/img_3.jpg").open
+end
+
 
--- db/fixtures/sample.rb
+++ db/fixtures/sample.rb
@@ -3,5 +3,6 @@ Sample.seed(
     [
       { id: 1, title: '川平湾', location_id: 1, image: 'Image1.jpg'},
       { id: 2, title: '宮古島牛が堪能できるレストラン（喜八)', location_id: 1, image: 'img_2.jpg'},
+      { id: 3, title: '竹富島で水牛体験', location_id: 1, image: 'img_3.jpg'},
     ]
   )
