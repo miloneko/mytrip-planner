d33fd7ee7384bd8472234c357bc7078a043042c4
miloneko <satohannn@gmail.com>
2022-07-12 10:25:38 +0900

    Add: 画像登録

--- db/fixtures/sample.rb
+++ db/fixtures/sample.rb
@@ -2,6 +2,6 @@ Sample.seed(
     :id,
     [
       { id: 1, title: '川平湾', location_id: 1, image: 'Image1.jpg'},
-      { id: 2, title: '宮古島牛が堪能できるレストラン（喜八)', location_id: 1, image: 'image_2.jpg'},
-     ]
+      { id: 2, title: '宮古島牛が堪能できるレストラン（喜八)', location_id: 1, image: 'img_2.jpg'},
+    ]
   )
