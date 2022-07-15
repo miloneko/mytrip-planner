195d1d666a69dc9649c04e6bf1fbdb2b740101eb
miloneko <satohannn@gmail.com>
2022-07-15 11:59:00 +0900

    Fix: HEICファイルでもアップロード可能

---
+++ db/fixtures/SampleCategory.rb
@@ -0,0 +1,7 @@
+SampleCategory.seed(
+    :id,
+    [
+      { sample_id: 1, category_id: 1},
+      { sample_id: 2, category_id: 3},
+    ]
+  )
--- db/fixtures/sample.rb
+++ db/fixtures/sample.rb
@@ -1,7 +1,7 @@
 Sample.seed(
     :id,
     [
-      { id: 1, title: '川平湾', location_id: 1, image: 'Image1.jpg'},
+      { id: 1, title: '川平湾', location_id: 1, image: 'Image1.jpg', sample_category_ids: 1},
       { id: 2, title: '宮古牛が堪能できるレストラン', location_id: 1, image: 'img_2.jpg'},
       { id: 3, title: '竹富島で水牛車体験', location_id: 1, image: 'img_3.jpg'}
     ]
